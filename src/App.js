f68dfb9233da414c6a078185ba0e39eae34ec7d9
seunjay <seunjay92@gmail.com>
2023-02-04 09:24:20 +0100

    add view collection button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,114 @@
-import './styles/App.css';
-import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import "./styles/App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import myEpicNft from "./utils/MyEpicNFT.json";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { Link } from "react-router-dom";
 
 // Constants
-const TWITTER_HANDLE = 'iamseunjay';
+const TWITTER_HANDLE = "iamseunjay";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
-const TOTAL_MINT_COUNT = 50;
+const OPENSEA_LINK =
+  "https://testnets.opensea.io/collection/squarenft-7kb1zv4ikz";
+// const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const checkIfWalletIsConnected = async () => {
+    /*
+     * First make sure we have access to window.ethereum
+     */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    /*
+     * Check if we're authorized to access the user's wallet
+     */
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+
+    /*
+     * User can have multiple authorized accounts, we grab the first one if its there!
+     */
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  };
+
+  /*
+   * Implement your connectWallet method here
+   */
+  // const connectWallet = async () => {
+  //   try {
+  //     const { ethereum } = window;
+
+  //     if (!ethereum) {
+  //       alert("Get MetaMask!");
+  //       return;
+  //     }
+
+  //     /*
+  //      * Fancy method to request access to account.
+  //      */
+  //     const accounts = await ethereum.request({
+  //       method: "eth_requestAccounts",
+  //     });
+
+  //     /*
+  //      * Boom! This should print out public address once we authorize Metamask.
+  //      */
+  //     console.log("Connected", accounts[0]);
+  //     setCurrentAccount(accounts[0]);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  const mintNFT = async () => {
+    const CONTRACT_ADDRESS = "0xAC5Bb476223445ffDdcf6173dC91D09354815D00";
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          myEpicNft.abi,
+          signer
+        );
+
+        console.log("Going to pop wallet now to pay gas...");
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log("Mining...please wait.");
+        await nftTxn.wait();
+
+        console.log(
+          `Mined, see transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`
+        );
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
@@ -24,7 +124,24 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            renderNotConnectedContainer()
+          ) : (
+            <button
+              onClick={mintNFT}
+              className="cta-button connect-wallet-button"
+            >
+              Mint NFT
+            </button>
+          )}
+        </div>
+
+        <div className="">
+          <Link to={OPENSEA_LINK}>
+            <button className="cta-button collection-link view-collection-button">
+              View Collection
+            </button>
+          </Link>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
